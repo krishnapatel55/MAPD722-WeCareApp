42e6b882e7681ea9a7d6c736bf98e29dd8e5b0f6
Deep Mehta <dmehta35@my.centennialcollege.ca>
2023-04-02 01:08:46 -0400

    Added unit test

---
+++ test/main_test.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_milestone_project/addPatientScreen.dart';
+import 'package:flutter_milestone_project/loginScreen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_milestone_project/main.dart';
+import 'package:flutter_milestone_project/constants.dart' as Constants;
+import 'package:http/http.dart' as http;
+
+void main() {
+  test("Some matcher test", () {
+    expect(const MyApp(), isA<MyApp>());
+  });
+
+  test("Patient API Response Test", () async {
+    final response = await http.get(Uri.parse('${Constants.urlPort}/patients'));
+    final data = json.decode(response.body);
+
+    expect(response.statusCode, 200);
+    expect(data, isNotEmpty);
+    expect(data[0]['_id'], isNotEmpty);
+    expect(data[0]['patient_name'], equals("Jenny"));
+    expect(data[0]['address'], isNotEmpty);
+    expect(data[0]['contact_no'], isNotEmpty);
+    expect(data[0]['age'], isNotEmpty);
+    expect(data[0]['gender'], isNotEmpty);
+    expect(data[0]['department'], isNotEmpty);
+    expect(data[0]['doctor'], isNotEmpty);
+    expect(data[0]['critical_condition'], true);
+  });
+
+  group('Login Screen TextForm Field', () {
+    Widget testwidget = MediaQuery(
+        data: const MediaQueryData(), child: MaterialApp(home: loginScreen()));
+    testWidgets('Render 2 TextField', (WidgetTester tester) async {
+      await tester.pumpWidget(testwidget);
+      expect(
+          find.byType(TextFormField),
+          findsNWidgets(
+              2)); // Expecting 2 TextFormField widget on Login Screen.
+    });
+  });
+
+  group('Main Screen listview Field', () {
+    Widget testwidget = const MediaQuery(
+        data: MediaQueryData(), child: MaterialApp(home: addPatientScreen()));
+    testWidgets('Render Listview', (WidgetTester tester) async {
+      await tester.pumpWidget(testwidget);
+      expect(find.byType(TextFormField),
+          findsWidgets); // Expecting ListView widget on main Screen.
+    });
+  });
+}
